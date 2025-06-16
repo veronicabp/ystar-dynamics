505d9cbd5f939fd8fb6ba18243214b7aa32e7b33
veronicabp <veronicabp@gmail.com>
2025-06-15 18:14:17 -0700

    bootstrap over treated properties

--- code/analysis/compile_bootstrap.py
+++ code/analysis/compile_bootstrap.py
@@ -6,47 +6,27 @@ def compile_bootstrap(data_folder):
     df = pd.read_pickle(os.path.join(data_folder, "clean", "experiments.p"))
 
     for boot_file in sorted(
-        os.listdir(os.path.join(data_folder, "working", "bootstrap"))
+        os.listdir(os.path.join(data_folder, "working", "bayes_bootstrap"))
     ):
         if not boot_file.endswith(".p"):
             continue
         boot = pd.read_pickle(
-            os.path.join(data_folder, "working", "bootstrap", boot_file)
+            os.path.join(data_folder, "working", "bayes_bootstrap", boot_file)
         ).drop_duplicates()
         df = df.merge(boot, on=["property_id", "date_trans"], how="inner")
 
-    baseline_ests = []
-    baseline_ses = []
     boot_ests = []
-    boot_ses = []
-    boot_sample_size = []
 
-    for boot_col in tqdm([col for col in df.columns if col.startswith("d_rsi_boot")]):
+    for i, boot_col in enumerate(
+        tqdm([col for col in df.columns if col.startswith("d_rsi_boot")])
+    ):
         df[f"di{boot_col}"] = df["d_log_price"] - df[boot_col]
         sub = df[df[f"di{boot_col}"].notna()].copy()
 
-        baseline_est, baseline_se = estimate_ystar(sub, get_se=True)
-        boot_est, boot_se = estimate_ystar(sub, lhs_var=f"di{boot_col}", get_se=False)
-
-        baseline_ests.append(baseline_est)
-        baseline_ses.append(baseline_se)
+        # Resample treated properties
+        boot_df = sub.sample(n=len(sub), replace=True, random_state=1111 + i)
+        boot_est, _ = estimate_ystar(boot_df, lhs_var=f"di{boot_col}", get_se=False)
 
         boot_ests.append(boot_est)
-        boot_ses.append(boot_se)
-
-        boot_sample_size.append(len(sub))
 
     print("Bootstrap SE:", np.std(boot_ests))
-    print("Mean Basline Robust SE:", np.mean(baseline_ses))
-    print("Mean Bootstrap Sample Size:", np.mean(boot_sample_size))
-
-    df = pd.DataFrame(
-        {
-            "baseline_est": baseline_ests,
-            "baseline_se": baseline_ses,
-            "boot_est": boot_ests,
-            "boot_se": boot_ses,
-            "boot_sample_size": boot_sample_size,
-        }
-    )
-    df.to_pickle(os.path.join(data_folder, "clean", "boostrap_estimates.p"))
